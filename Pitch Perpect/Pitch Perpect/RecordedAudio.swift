0ed46789d96c3c42e6fa8c58b470421acd98b484
joseftan <jt4900@att.com>
2015-03-30 22:04:21 -0700

    Committed by Udacity

---
+++ Pitch Perpect/Pitch Perpect/RecordedAudio.swift
@@ -0,0 +1,13 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perpect
+//
+//  Created by Joseph Tan on 3/29/15.
+//  Copyright (c) 2015 Joseph Tan. All rights reserved.
+//
+
+import Foundation
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
